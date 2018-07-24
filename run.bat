fd1182b472c303b96ed087dce1631a905ec1c9b0
Mike M <menkom@users.noreply.github.com>
2018-07-24 23:28:39 +0300

    Edited batch file with new jar modules.

---
+++ run.bat
@@ -0,0 +1,2 @@
+@java -cp ./lib/textFileWorker.jar;./lib/datahelp.jar;./lib/exec.jar;./lib/facade.jar;./lib/model.jar;./lib/rep.jar;./lib/service.jar;./lib/util.jar  com.senla.exec.Executive ./db/
+@pause
