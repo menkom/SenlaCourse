b3a50e15e9af1b391d07bc2e8d6a50ffe47a3271
menko.mikhail <menko.mihail@gmail.com>
2018-08-14 21:38:40 +0300

    Export util for model

--- controller/src/com/senla/util/ExportCSV.java
+++ controller/src/com/senla/util/ExportCSV.java
@@ -10,6 +10,9 @@ import java.util.List;
 import com.senla.converter.ListConverter;
 import com.senla.exception.NoEntryException;
 import com.senla.hotel.model.Client;
+import com.senla.hotel.model.Order;
+import com.senla.hotel.model.Room;
+import com.senla.hotel.model.Service;
 
 public class ExportCSV {
 
@@ -44,4 +47,30 @@ public class ExportCSV {
 		return result;
 	}
 
+	public static List<Order> getOrdersFromCSV(String file) throws NoEntryException, IOException {
+		List<Order> result = new ArrayList<>();
+		List<String> list = loadCSV(file);
+		if (list != null) {
+			result = ListConverter.getOrders(list);
+		}
+		return result;
+	}
+
+	public static List<Room> getRoomsFromCSV(String file) throws NoEntryException, IOException {
+		List<Room> result = new ArrayList<>();
+		List<String> list = loadCSV(file);
+		if (list != null) {
+			result = ListConverter.getRooms(list);
+		}
+		return result;
+	}
+
+	public static List<Service> getServicesFromCSV(String file) throws NoEntryException, IOException {
+		List<Service> result = new ArrayList<>();
+		List<String> list = loadCSV(file);
+		if (list != null) {
+			result = ListConverter.getServices(list);
+		}
+		return result;
+	}
 }
