ffd1afc37c4c46e79b8a51884bb0a3834df0aac0
menko.mikhail <menko.mihail@gmail.com>
2018-08-14 22:13:44 +0300

    Import CSV actions

---
+++ ui/src/com/senla/ui/action/service/ImportService.java
@@ -0,0 +1,32 @@
+package com.senla.ui.action.service;
+
+import com.senla.hotel.facade.Hotel;
+import com.senla.ui.base.IAction;
+import com.senla.ui.util.FileCheck;
+import com.senla.ui.util.Input;
+import com.senla.util.DisplayOperator;
+
+public class ImportService implements IAction {
+
+	private static final String ENTER_FILE_NAME = "Enter file name to import services: ";
+	private static final String FILE_IMPORT = "File %s imported.";
+	private static final String FILE_IMPORT_FAILED = "Error during services import.";
+	private static final String FILE_NOT_EXIST = "File %s not exist.";
+
+	@Override
+	public void execute() {
+
+		DisplayOperator.printMessage(ENTER_FILE_NAME);
+		String fileName = Input.inputString();
+		if (FileCheck.isFileExist(fileName)) {
+			Boolean result = Hotel.getInstance().importServicesCSV(fileName);
+			if (result) {
+				DisplayOperator.printMessage(String.format(FILE_IMPORT, fileName));
+			} else {
+				DisplayOperator.printMessage(FILE_IMPORT_FAILED);
+			}
+		} else {
+			DisplayOperator.printMessage(String.format(FILE_NOT_EXIST, fileName));
+		}
+	}
+}
