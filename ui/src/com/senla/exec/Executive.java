ab0ea4750971aa201f2245e877a5431e557b0d85
netlynx <menko.mihail@gmail.com>
2018-08-25 08:23:23 +0300

    Moved to csv data source.

--- ui/src/com/senla/exec/Executive.java
+++ ui/src/com/senla/exec/Executive.java
@@ -1,6 +1,7 @@
 package com.senla.exec;
 
 import com.senla.hotel.facade.Hotel;
+import com.senla.ui.base.MenuController;
 import com.senla.ui.util.DisplayOperator;
 
 public class Executive {
@@ -9,10 +10,11 @@ public class Executive {
 	private static final String ERROR_SAVE = "Error while data saving.";
 
 	public static void main(String[] args) {
-		Boolean loaded = Hotel.getInstance().load();
-		if (!loaded) {
-			DisplayOperator.printMessage(ERROR_LOAD);
-		}
+		Boolean loaded;
+//		loaded = Hotel.getInstance().load();
+//		if (!loaded) {
+//			DisplayOperator.printMessage(ERROR_LOAD);
+//		}
 
 		loaded = Hotel.getInstance().importCsv();
 
@@ -22,8 +24,8 @@ public class Executive {
 			DisplayOperator.printMessage("Csv loaded.");
 		}
 
-//		MenuController menuController = new MenuController();
-//		menuController.run();
+		MenuController menuController = new MenuController();
+		menuController.run();
 
 		Boolean saved = Hotel.getInstance().save();
 		if (!saved) {
