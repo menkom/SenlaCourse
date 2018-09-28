df320475a618c1ac5bb24981e5d28885fcea2637
netlynx <menko.mihail@gmail.com>
2018-09-28 11:58:25 +0300

    added some PCs

--- sql/insert.sql
+++ sql/insert.sql
@@ -47,3 +47,11 @@ INSERT INTO `laptop` (`code`, `model`, `speed`, `ram`, `hd`, `price`, `screen`) 
 INSERT INTO `pc` (`code`, `model`, `speed`, `ram`, `hd`, `cd`, `price`) VALUES (NULL, '8300 Elite', '3200', '8000', '500', '0', '184');
 INSERT INTO `pc` (`code`, `model`, `speed`, `ram`, `hd`, `cd`, `price`) VALUES (NULL, 'Aspire Desktop', '2400', '8000', '1000', '8', '700');
 INSERT INTO `pc` (`code`, `model`, `speed`, `ram`, `hd`, `cd`, `price`) VALUES (NULL, 'Compaq CQ5210F', '4000', '3000', '500', '0', '400');
+
+INSERT INTO `pc` (`code`, `model`, `speed`, `ram`, `hd`, `cd`, `price`) VALUES
+(NULL, '2017 HP Pavilion Slimline 400 Flagship Premium', '1400', '4', '500', '24', '380'),
+(NULL, '24-r028ur', '2900', '4000', '1000', '24', '1100'),
+(NULL, 'Aspire Desktop', '3500', '12000', '2000', '8', '940'),
+(NULL, 'Optiplex 780 Minitower', '3000', '4000', '160', '12', '220'),
+(NULL, 'dc5800', '3000', '2000', '160', '48', '2400'),
+(NULL, 'presario CQ5300', '2300', '2000', '320', '24', '150');
