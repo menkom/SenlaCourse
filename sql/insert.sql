efee02c9f4dce1756ca9a8d138f099ca82024cb7
netlynx <menko.mihail@gmail.com>
2018-09-29 14:52:18 +0300

    Added some laptops

--- sql/insert.sql
+++ sql/insert.sql
@@ -10,6 +10,17 @@ INSERT INTO product (maker, model, type) VALUES ('Toshiba', 'Satellite P755', 'l
 INSERT INTO product (maker, model, type) VALUES ('Asus', 'X555', 'laptop');
 INSERT INTO product (maker, model, type) VALUES ('Asus', 'X751', 'laptop');
 
+INSERT INTO product (maker, model, type) VALUES 
+('ASUS', 'C302CA-DHM4 Chromebook','laptop'),
+('ASUS','ZenBook','laptop'),
+('Eluktronics', 'N850HK1 Pro Gaming', 'laptop'),
+('HP', '17','laptop'),
+('CUK', 'Probook 450 G5', 'laptop'),
+('Dell', 'Latitude 14 5000 5414','laptop'),
+('Lenovo', 'Thinkpad Yoga 260','laptop'),
+('Samsung', 'Notebook 9 Pro','laptop'),
+('Samsung', 'Notebook Odyssey', 'laptop'),
+('Lenovo','Miix 630','laptop');
 
 INSERT INTO product (maker, model, type) VALUES ('hp', 'dc5800', 'pc');
 INSERT INTO product (maker, model, type) VALUES ('hp', '24-r028ur','pc');
@@ -43,6 +54,19 @@ INSERT INTO `laptop` (`code`, `model`, `speed`, `ram`, `hd`, `price`, `screen`)
 INSERT INTO `laptop` (`code`, `model`, `speed`, `ram`, `hd`, `price`, `screen`) VALUES (NULL, 'x250', '1500', '4000', '256', '600', '12');
 INSERT INTO `laptop` (`code`, `model`, `speed`, `ram`, `hd`, `price`, `screen`) VALUES (NULL, 'EliteBook 2560p', '3000', '4000', '2128', '400', '12');
 
+INSERT INTO laptop (model, speed, ram, hd, price, screen) VALUES 
+  ('C302CA-DHM4 Chromebook', '2200', '4000', '64', '950', '12'),
+  ('ZenBook', '1600', '8000', '256', '1446', '13'),
+  ('N850HK1 Pro Gaming', '2800', '16000', '512', '2000', '15'),
+  ('17', '2500', '8000', '1000', '960', '17'),
+  ('Probook 450 G5', '1800', '32000', '2500', '2540', '15'),
+  ('Latitude 14 5000 5414', '2400', '8000', '128', '1760', '14'),
+  ('Inspiron 15', '2500', '8000', '1000', '1040', '15'),
+  ('Thinkpad Yoga 260', '2300', '8000', '500', '1900', '12'),
+  ('Notebook 9 Pro', '4000', '8000', '2000', '3800', '13'),
+  ('Notebook Odyssey', '3800', '16000', '500', '3200', '15'),
+  ('vaio', '2000', '4000', '250', '710', '16'),
+  ('Miix 630', '1900', '4000', '250', '1680', '12');
 
 INSERT INTO `pc` (`code`, `model`, `speed`, `ram`, `hd`, `cd`, `price`) VALUES (NULL, '8300 Elite', '3200', '8000', '500', '0', '184');
 INSERT INTO `pc` (`code`, `model`, `speed`, `ram`, `hd`, `cd`, `price`) VALUES (NULL, 'Aspire Desktop', '2400', '8000', '1000', '8', '700');
