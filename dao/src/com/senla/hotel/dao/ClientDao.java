73ed5660bf0ecc550f160a166783b28706397cd7
netlynx <menko.mihail@gmail.com>
2018-10-03 00:44:09 +0300

    All the methods are done. To complete we need to make di work with private constructor

--- dao/src/com/senla/hotel/dao/ClientDao.java
+++ dao/src/com/senla/hotel/dao/ClientDao.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.senla.annotation.parser.CsvParser;
 import com.senla.dao.DaoHandler;
+import com.senla.di.DependencyInjection;
 import com.senla.hotel.model.Client;
 import com.senla.hotel.repository.api.IClientRepository;
 
@@ -16,34 +18,92 @@ public class ClientDao implements IClientRepository {
 
 	private static final Logger logger = Logger.getLogger(ClientDao.class);
 
+	private static IClientRepository clientRepository;
+
+	private ClientDao() {
+		super();
+	}
+
+	public static IClientRepository getInstance() {
+		if (clientRepository == null) {
+			clientRepository = DependencyInjection.getInstance().getInterfacePair(IClientRepository.class);
+		}
+		return clientRepository;
+	}
+
 	@Override
 	public boolean add(Client client) {
-		// TODO Auto-generated method stub
-		return false;
+		int result = 0;
+		try {
+			result = DaoHandler.getInstance().executeUpdate(
+					"insert into client (id, name) values (" + client.getId() + ",\'" + client.getName() + "\')");
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+		return result > 0;
 	}
 
 	@Override
 	public boolean addAll(List<Client> clients) {
-		// TODO Auto-generated method stub
-		return false;
+		int result = 0;
+		if (clients.size() > 0) {
+			StringBuilder query = new StringBuilder("insert into client (id, name) values ");
+			for (Client client : clients) {
+				query.append("(" + client.getId() + ",\'" + client.getName() + "\'),");
+			}
+			query.deleteCharAt(query.length() - 1);
+			try {
+				result = DaoHandler.getInstance().executeUpdate(query.toString());
+			} catch (SQLException e) {
+				logger.error(e);
+			}
+		}
+		System.out.println("addAll result:" + result);
+		return result > 0;
 	}
 
 	@Override
 	public boolean delete(String name) {
-		// TODO Auto-generated method stub
-		return false;
+		int result = 0;
+		try {
+			result = DaoHandler.getInstance().executeUpdate("delete client where name=\'" + name + "\'");
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+		return result > 0;
 	}
 
 	@Override
 	public boolean deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
+		int result = 0;
+		try {
+			result = DaoHandler.getInstance().executeUpdate("delete client  where id=\'" + id + "\'");
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+		return result > 0;
 	}
 
 	@Override
 	public Client getClientByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Client client = null;
+		ResultSet resultSet = null;
+		try {
+			resultSet = DaoHandler.getInstance().executeQuery("select * from client where name=\'" + name + "\'");
+
+			while (resultSet.next()) {
+				client = new Client();
+				client.setId(resultSet.getInt("id"));
+				client.setName(resultSet.getString("name"));
+
+				System.out.println("client by id : " + client);
+
+				return client;
+			}
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+		return client;
 	}
 
 	@Override
@@ -51,14 +111,14 @@ public class ClientDao implements IClientRepository {
 		Client client = null;
 		ResultSet resultSet = null;
 		try {
-			resultSet = DaoHandler.getInstance().executeQuery("select * from client");
+			resultSet = DaoHandler.getInstance().executeQuery("select * from client where id=\'" + id + "\'");
 
 			while (resultSet.next()) {
 				client = new Client();
 				client.setId(resultSet.getInt("id"));
 				client.setName(resultSet.getString("name"));
 
-				System.out.println("client : " + client);
+				System.out.println("client by id : " + client);
 
 				return client;
 			}
@@ -105,14 +165,12 @@ public class ClientDao implements IClientRepository {
 
 	@Override
 	public boolean exportCsv(String csvFilePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
+		return CsvParser.exportToCsv(getClients(), csvFilePath);
 	}
 
 	@Override
 	public boolean importCsv(String csvFilePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
+		return addAll(CsvParser.importFromCsv(Client.class, csvFilePath));
 	}
 
 }
