6f540a50123020d48415a455c9e2ff6030dd829d
netlynx <menko.mihail@gmail.com>
2018-10-03 22:06:39 +0300

    It was working before with public constructor only. But according to algorithm Singleton was not broken.

--- di/src/com/senla/di/DependencyInjection.java
+++ di/src/com/senla/di/DependencyInjection.java
@@ -3,6 +3,8 @@ package com.senla.di;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -11,12 +13,15 @@ import org.apache.log4j.Logger;
 
 public class DependencyInjection {
 
+	private static final String ERROR_INVOKED_METHOD = "Exception thrown by an invoked method or constructor.";
+	private static final String ERROR_ILLEGAL_ARGUMENT = "Error. Method has been passed an illegal or inappropriate argument.";
+
 	private static final Logger logger = Logger.getLogger(DependencyInjection.class);
 
 	private static final String ERROR_DEPENDENCY_INJECTION_LOADING = "Error dependency injection property file load.";
 	private static final String ERROR_DEPENDENCY_INJECTION_NO_FILE = "Error. Dependency injection property file not found.";
 	private static final String ERROR_DEPENDENCY_INJECTION_NO_PAIR = "Such dependency pair not found.";
-	private static final String ERROR_DEPENDENCY_INJECTION_NEW_INSTANCE = "Error during instance cretion using dependency injection.";
+	private static final String ERROR_DEPENDENCY_INJECTION_NEW_INSTANCE = "Error during instance creation using dependency injection.";
 
 	private static final String INSTANCE_PATH = "./dependency.properties";
 	private static DependencyInjection instance;
@@ -66,16 +71,24 @@ public class DependencyInjection {
 				logger.error(ERROR_DEPENDENCY_INJECTION_NO_PAIR);
 			}
 			if (!instances.containsKey(cl)) {
-				Object result = cl.newInstance();
+				Constructor<?>[] constr = cl.getDeclaredConstructors();
+				constr[0].setAccessible(true);
+				Object result = constr[0].newInstance();
+				constr[0].setAccessible(false);
 				instances.put(cl, result);
 			}
-
 			return (T) instances.get(cl);
 		} catch (ClassNotFoundException e) {
 			logger.error(ERROR_DEPENDENCY_INJECTION_NO_PAIR, e);
 			return null;
 		} catch (InstantiationException | IllegalAccessException e) {
 			logger.error(ERROR_DEPENDENCY_INJECTION_NEW_INSTANCE, e);
+			return null;
+		} catch (IllegalArgumentException e) {
+			logger.error(ERROR_ILLEGAL_ARGUMENT, e);
+			return null;
+		} catch (InvocationTargetException e) {
+			logger.error(ERROR_INVOKED_METHOD, e);
 			return null;
 		}
 	}
