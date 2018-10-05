92405f0ab8d45f312781c30be0f7715588c8199c
netlynx <menko.mihail@gmail.com>
2018-10-05 22:56:38 +0300

    Testing Executer. Now for OrderDao

--- dao/src/com/senla/dao/Executer.java
+++ dao/src/com/senla/dao/Executer.java
@@ -1,13 +1,13 @@
 package com.senla.dao;
 
-import com.senla.dao.test.ServiceTest;
+import com.senla.dao.test.OrderTest;
 
 public class Executer {
 
 	public static void main(String[] args) {
-
 //		ClientTest.testClientDao();
 //		RoomTest.testRoomDao();
-		ServiceTest.testServiceDao();
+//		ServiceTest.testServiceDao();
+		OrderTest.testOrderDao();
 	}
 }
