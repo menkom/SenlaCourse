ad5709fe8cea3f29a7a2b4d2f7e0964cfafdb7b5
netlynx <menko.mihail@gmail.com>
2018-10-22 04:42:24 +0300

    Closing SessionFactory

--- hotel/ui/src/main/java/com/senla/exec/Executive.java
+++ hotel/ui/src/main/java/com/senla/exec/Executive.java
@@ -1,14 +1,17 @@
 package com.senla.exec;
 
+import com.senla.hotel.facade.Hotel;
 import com.senla.ui.base.MenuController;
 
 public class Executive {
 
 	public static void main(String[] args) {
-
-		MenuController menuController = new MenuController();
-		menuController.run();
-
+		try {
+			MenuController menuController = new MenuController();
+			menuController.run();
+		} finally {
+			Hotel.getInstance().close();
+		}
 	}
 
 }
