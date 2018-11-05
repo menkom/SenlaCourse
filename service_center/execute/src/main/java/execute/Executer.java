c0e6232503336d99fc027bda8ea9b52b7f4b665d
netlynx <menko.mihail@gmail.com>
2018-11-05 22:58:05 +0300

    Modified for testing

--- service_center/execute/src/main/java/execute/Executer.java
+++ service_center/execute/src/main/java/execute/Executer.java
@@ -1,5 +1,6 @@
 package execute;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
 import dao.HibernateUtil;
@@ -7,27 +8,33 @@ import model.PartCategory;
 
 public class Executer {
 
+	private static final Logger logger = Logger.getLogger(Executer.class);
+
 	public static void main(String[] args) {
 
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
+		Session session = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			session.beginTransaction();
 
-		// Check database version
-		String sql = "select version()";
+			PartCategory partCategory = new PartCategory();
+			partCategory.setName("NameCat");
 
-		String result = (String) session.createNativeQuery(sql).getSingleResult();
-		System.out.println(result);
+			session.save(partCategory);
 
-		//Testing models
-		PartCategory partCategory = new PartCategory();
-		partCategory.setName("NameCat");
-		
-		
-		session.getTransaction().commit();
-		session.close();
-
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			if (session != null) {
+				session.getTransaction().rollback();
+			}
+			logger.error(e);
+			throw e;
+		} finally {
+			if ((session != null) && (session.isOpen())) {
+				session.close();
+			}
+		}
 		HibernateUtil.shutdown();
-	
 	}
 
 }
