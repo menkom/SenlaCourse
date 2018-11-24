1c27ceb3ffc34f874fca90f9a9c6d7de9568c321
netlynx <menko.mihail@gmail.com>
2018-11-24 23:26:45 +0300

    Testing bean for jsf pages

---
+++ service_center/webapp/src/main/java/info/mastera/CustomerBean.java
@@ -0,0 +1,60 @@
+package info.mastera;
+
+import info.mastera.model.Customer;
+import info.mastera.service.ICustomerService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+
+@Component
+@ManagedBean
+@SessionScoped
+public class CustomerBean {
+
+    private static final Logger logger = Logger.getLogger(CustomerBean.class);
+
+    @Autowired
+    private ICustomerService customerService;
+
+    private String customerName;
+    private String customerPhone;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public void create() {
+        Customer customer = new Customer();
+        customer.setCustomerName(customerName);
+        customer.setTelephone(customerPhone);
+        customerService.create(customer);
+        clearForm();
+    }
+
+    private void clearForm() {
+        setCustomerName("");
+        setCustomerPhone("");
+    }
+
+    public List<Customer> getAll() {
+        logger.info("Abstract.getAll; " + this.getClass());
+        return customerService.getAll();
+    }
+
+}
