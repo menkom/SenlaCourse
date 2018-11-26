62ff63e3640fdbe3691462d60e6e617d2f510317
netlynx <menko.mihail@gmail.com>
2018-11-27 02:31:20 +0300

    Testing User class in console version

--- service_center/db/src/main/java/info/mastera/TestExecuter.java
+++ service_center/db/src/main/java/info/mastera/TestExecuter.java
@@ -2,7 +2,10 @@ package info.mastera;
 
 import info.mastera.config.HibernateConfig;
 import info.mastera.model.Customer;
+import info.mastera.model.User;
+import info.mastera.model.enums.UserType;
 import info.mastera.service.ICustomerService;
+import info.mastera.service.IUserService;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,44 +15,97 @@ public class TestExecuter {
 
     private static final Logger logger = Logger.getLogger(TestExecuter.class);
 
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
+    private static void testCustomer(AnnotationConfigApplicationContext context) {
 
-        ICustomerService customerService = context.getBean(ICustomerService.class);
+        ICustomerService<Customer> customerService = context.getBean(ICustomerService.class);
 
         Customer customer = new Customer();
         customer.setCustomerName("custName");
         customer.setTelephone("phoneNum");
 
-        printCustomer(customerService.create(customer));
+        print(customerService.create(customer));
 
         List<Customer> customers = customerService.getAll();
 
         logger.info("Customers: " + customers);
 
-//        printCustomer(customerService.getById(11));
+        print(customerService.getById(11));
 
-//        customers.forEach(cust -> {
-//            cust.setCustomerName(cust.getCustomerName() + cust.getId());
-//            cust.setTelephone(cust.getTelephone() + cust.getId());
-//            customerService.update(cust);
-//        });
+        customers.forEach(cust -> {
+            cust.setCustomerName(cust.getCustomerName() + cust.getId());
+            cust.setTelephone(cust.getTelephone() + cust.getId());
+            customerService.update(cust);
+        });
 
-        customers.forEach(TestExecuter::printCustomer);
+        customers.forEach(TestExecuter::print);
 
         if (customerService.getById(1) != null) {
             customerService.delete(customerService.getById(1));
         }
-        customers.forEach(TestExecuter::printCustomer);
+        customers.forEach(TestExecuter::print);
 
+
+        logger.info("Customer operations passed.");
+    }
+
+    private static void testUser(AnnotationConfigApplicationContext context) {
+
+        IUserService<User> service = context.getBean(IUserService.class);
+
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword("7pas");
+        user.setUserType(UserType.CUSTOMER);
+
+        ICustomerService<Customer> customerService = context.getBean(ICustomerService.class);
+
+        user.setCustomer(customerService.getById(7));
+
+        print(service.create(user));
+
+        List<User> all = service.getAll();
+
+        logger.info("users: " + user);
+
+        print(service.getById(3));
+
+//        all.forEach(x -> {
+//            x.setUsername(x.getUsername() + x.getId());
+//            service.update(x);
+//        });
+
+//        all.forEach(TestExecuter::print);
+
+//        if (service.getById(2) != null) {
+//            service.delete(service.getById(2));
+//        }
+        all.forEach(TestExecuter::print);
+
+
+        logger.info("User operations passed.");
+    }
+
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
+
+        testUser(context);
 
         logger.info("Job is done");
     }
 
-    private static void printCustomer(Customer customer) {
+    private static void print(Customer customer) {
         if (customer != null) {
             logger.info(customer.getId() + "  |  " + customer.getCustomerName() + "  |  " + customer.getTelephone());
         }
     }
 
+    private static void print(User user) {
+        if (user != null) {
+            logger.info(user.getId() + "  |  " + user.getUsername()
+                    + "  |  " + user.getPassword() + "  |  "
+                    + user.getUserType() + "  |  "
+                    + (user.getCustomer() != null ? user.getCustomer().getId() : "null"));
+        }
+    }
 }
