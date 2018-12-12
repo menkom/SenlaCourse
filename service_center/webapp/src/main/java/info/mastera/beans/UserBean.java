74505dbd562513d9bb5b7c51fd0fa4276ec9a9fc
netlynx <menko.mihail@gmail.com>
2018-12-12 11:23:15 +0300

    Changed from work with fields in bean for every field in page to whole object in bean

--- service_center/webapp/src/main/java/info/mastera/beans/UserBean.java
+++ service_center/webapp/src/main/java/info/mastera/beans/UserBean.java
@@ -1,13 +1,14 @@
-package info.mastera;
+package info.mastera.beans;
 
-import info.mastera.model.Customer;
 import info.mastera.model.User;
-import info.mastera.model.enums.UserType;
 import info.mastera.service.IGenericService;
 import info.mastera.service.IUserService;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -17,75 +18,67 @@ public class UserBean extends GenericBean<User> {
 
     @Inject
     private IUserService<User> userService;
+
     private static final Logger logger = Logger.getLogger(UserBean.class);
 
-    private Integer userId;
-    private String username;
-    private String password;
-    private Customer customer;
-    private UserType userType;
+    private User user;
 
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
-
-    @Override
-    public void create() {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setCustomer(customer);
-        user.setUserType(userType);
-        getService().create(user);
-        clearForm();
+    @PostConstruct
+    private void init() {
+        user = new User();
     }
 
     @Override
     protected void clearForm() {
-        username = "";
-        password = "";
-        customer = null;
-        userType = UserType.CUSTOMER;
+        user = new User();
     }
 
     @Override
     protected IGenericService<User> getService() {
         return userService;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        user = null;
+    }
+
+
+    @Override
+    public void create() {
+        User user = new User();
+        getService().create(user);
+        clearForm();
+    }
+
+    public String login() {
+        if (userService.isCorrectLogin(user.getUsername(), user.getPassword())) {
+            logger.info("views/loginSuccess?faces-redirect=true");
+            jwtOperator.setAuthentication(FacesContext.getCurrentInstance(), user);
+            clearForm();
+            //TODO Clear form after logging
+            //TODO Create logout
+            return "views/mainPage?faces-redirect=true";
+        } else {
+            //TODO Correct redirect or correct user informing
+            logger.info("!!loginFailed?faces-redirect=true!!");
+            return "views/loginFailed?faces-redirect=true";
+        }
+    }
+
+    public String logout() {
+//TODO Add check is it really log in
+
+        jwtOperator.logout(FacesContext.getCurrentInstance());
+        return "index?faces-redirect=true";
+    }
+
 }
