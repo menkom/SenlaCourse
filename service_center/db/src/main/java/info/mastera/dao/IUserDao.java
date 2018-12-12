0d2365fb29a95eea12b33383b175af6b7ecab398
netlynx <menko.mihail@gmail.com>
2018-12-12 10:33:02 +0300

    Added   1. getByUsername
      2. isCorrectLogin

--- service_center/db/src/main/java/info/mastera/dao/IUserDao.java
+++ service_center/db/src/main/java/info/mastera/dao/IUserDao.java
@@ -4,4 +4,8 @@ import info.mastera.model.User;
 
 public interface IUserDao<T extends User> extends IGenericDao<T> {
 
+    User getByUsername(String userName);
+
+    boolean isCorrectLogin(String userName, String password);
+
 }
--- service_center/db/src/main/java/info/mastera/dao/impl/UserDao.java
+++ service_center/db/src/main/java/info/mastera/dao/impl/UserDao.java
@@ -2,7 +2,15 @@ package info.mastera.dao.impl;
 
 import info.mastera.dao.IUserDao;
 import info.mastera.model.User;
+import info.mastera.model.User_;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Repository
 public class UserDao extends AbstractDao<User> implements IUserDao<User> {
@@ -10,4 +18,31 @@ public class UserDao extends AbstractDao<User> implements IUserDao<User> {
     protected Class<User> getTClass() {
         return User.class;
     }
+
+    @Override
+    public User getByUsername(String userName) {
+        Session session = getSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.select(root).where(builder.equal(root.get(User_.username), userName));
+        TypedQuery<User> result = session.createQuery(query);
+        User user = ((Query<User>) result).uniqueResult();
+        return user;
+    }
+
+    @Override
+    public boolean isCorrectLogin(String userName, String password) {
+        Session session = getSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.select(root)
+                .where(builder.equal(root.get(User_.username), userName),
+                        builder.equal(root.get(User_.password), password));
+        TypedQuery<User> result = session.createQuery(query);
+        User user = ((Query<User>) result).uniqueResult();
+        return user != null;
+    }
+
 }
