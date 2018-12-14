dd242e3292095445603789c2ad4353052422b72e
netlynx <menko.mihail@gmail.com>
2018-12-15 00:17:53 +0300

    Noticed that annotation at this classes is useless

--- service_center/db/src/main/java/info/mastera/dao/ICustomerDao.java
+++ service_center/db/src/main/java/info/mastera/dao/ICustomerDao.java
@@ -1,10 +1,7 @@
 package info.mastera.dao;
 
-import org.springframework.stereotype.Component;
-
 import info.mastera.model.Customer;
 
-@Component
 public interface ICustomerDao<T extends Customer> extends IGenericDao<T> {
 
 }
--- service_center/db/src/main/java/info/mastera/service/ICustomerService.java
+++ service_center/db/src/main/java/info/mastera/service/ICustomerService.java
@@ -1,12 +1,7 @@
 package info.mastera.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-
 import info.mastera.model.Customer;
 
-@Component
 public interface ICustomerService<T extends Customer> extends IGenericService<T> {
 
 }
