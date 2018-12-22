ccd6b303615984881f1a839c0d89ebeba903b97d
netlynx <menko.mihail@gmail.com>
2018-12-22 10:53:34 +0300

    After deleting abstract service

--- service_center/web/src/main/java/info/mastera/beans/customer/CustomerListBean.java
+++ service_center/web/src/main/java/info/mastera/beans/customer/CustomerListBean.java
@@ -3,21 +3,38 @@ package info.mastera.beans.customer;
 import info.mastera.beans.base.BaseListBean;
 import info.mastera.model.Customer;
 import info.mastera.service.ICustomerService;
-import info.mastera.service.IGenericService;
 import org.springframework.context.annotation.Scope;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 
 @Named
 @Scope("session")
 public class CustomerListBean extends BaseListBean<Customer> {
 
     @Inject
-    private ICustomerService<Customer> customerService;
+    private ICustomerService customerService;
+
 
     @Override
-    protected IGenericService<Customer> getService() {
-        return customerService;
+    public List<Customer> getAll() {
+        //TODO Use Lazy method instead
+        return customerService.getAll();
     }
+
+    @Override
+    public void delete() {
+        customerService.delete(getSelectedItem());
+        if (getSelectedItem() != null) {
+           addMessage(String.format(MESSAGE_ITEM_DELETED, getSelectedItem().getId()));
+        }
+        clearSelected();
+    }
+
+    @Override
+    public void update() {
+        customerService.update(getSelectedItem());
+    }
+
 }
--- service_center/web/src/main/java/info/mastera/beans/user/UserListBean.java
+++ service_center/web/src/main/java/info/mastera/beans/user/UserListBean.java
@@ -2,22 +2,38 @@ package info.mastera.beans.user;
 
 import info.mastera.beans.base.BaseListBean;
 import info.mastera.model.User;
-import info.mastera.service.IGenericService;
 import info.mastera.service.IUserService;
 
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
 
 @Named
 @SessionScoped
 public class UserListBean extends BaseListBean<User> {
 
     @Inject
-    private IUserService<User> userService;
+    private IUserService userService;
 
     @Override
-    protected IGenericService<User> getService() {
-        return userService;
+    public List<User> getAll() {
+        //TODO Use Lazy method instead
+        return userService.getAll();
     }
+
+    @Override
+    public void delete() {
+        userService.delete(getSelectedItem());
+        if (getSelectedItem() != null) {
+            addMessage(String.format(MESSAGE_ITEM_DELETED, getSelectedItem().getId()));
+        }
+        clearSelected();
+    }
+
+    @Override
+    public void update() {
+        userService.update(getSelectedItem());
+    }
+
 }
